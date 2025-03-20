dedee6535e1ddb2b25d756af05008ae4b1138e31
Riviera <riviera613@163.com>
2025-03-20 16:19:31 +0800

    fix: update settings

--- env_protect/settings.py
+++ env_protect/settings.py
@@ -140,6 +140,10 @@ STATIC_ROOT = BASE_DIR.joinpath('static/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+log_dir = os.path.join(BASE_DIR, 'logs')
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
